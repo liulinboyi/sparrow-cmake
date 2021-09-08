37abdb16a1c2324889e44e0af1d3e6985abc63f2
liulinboyi <xiaoliumail@foxmail.com>
2021-09-08 16:33:21 +0800

    feat: add range comments

--- src/object/obj_range.c
+++ src/object/obj_range.c
@@ -5,9 +5,9 @@
 
 //新建range对象
 ObjRange* newObjRange(VM* vm, int from, int to) {
-   ObjRange* objRange = ALLOCATE(vm, ObjRange);
-   initObjHeader(vm, &objRange->objHeader, OT_RANGE, vm->rangeClass);
-   objRange->from = from;
-   objRange->to = to;
-   return objRange;
+   ObjRange* objRange = ALLOCATE(vm, ObjRange); // 分配内存
+   initObjHeader(vm, &objRange->objHeader, OT_RANGE, vm->rangeClass); // 初始化对象头
+   objRange->from = from; // 初始化对象的from
+   objRange->to = to; // 初始化对象的to
+   return objRange; // 最后返回
 }
